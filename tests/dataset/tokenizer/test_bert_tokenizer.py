6859eb47c5a68633d7af25974dc650988055158e
yingyuankai <yingyuankai@live.com>
2020-03-01 23:07:31 +0800

    fix bugs

---
+++ tests/dataset/tokenizer/test_bert_tokenizer.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestBertTokenizer(unittest.TestCase):
+    def test_vocab_from_(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
