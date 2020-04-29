ace7cea8c71096aeaf56779742b15a90a49ff4a7
yingyuankai <yingyuankai@sogou-inc.com>
2020-04-29 17:04:19 +0800

    fix bugs

--- scrpts/start_training_duee_role_as_qa.sh
+++ scrpts/start_training_duee_role_as_qa.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=5,6
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 nohup python -u aispace/trainer.py \
     --experiment_name role \
     --model_name bert_for_qa \
     --schedule train_and_eval \
     --config_name DuEE_role_as_qa \
     --config_dir ./configs/2020_LSTC \
-    --gpus 0 1 \
+    --gpus 0 1 2 3 \
     > err.log 2>&1 &
 
 
